6633f27f96d8e312d43f73175395e1a38f8d2667
christinezhangg <54513295+christinezhangg@users.noreply.github.com>
2022-12-15 12:28:54 -0600

    i am just so good at coding wow

--- script.js
+++ script.js
@@ -1,1 +1,80 @@
+document.getElementById("NewAdviceButton").addEventListener("click", newAdvice);
+document.getElementById("ViewSavedAdviceButton").addEventListener("click", savedAdvice);
+document.getElementById("AbouttheCreatorButton").addEventListener("click", goAbout);
+document.getElementById("gobackhome").addEventListener("click", goHome);
+document.getElementById("gobackhomey").addEventListener("click", goHome);
+document.getElementById("gobackhomeyy").addEventListener("click", goHome);
+var url = "https://api.adviceslip.com/advice";
+var advicee = [];
 
+document.getElementById("NewAdvicePage").style.display = "none";
+document.getElementById("SavedAdvicePage").style.display = "none";
+document.getElementById("AboutPage").style.display = "none";
+
+function getAdvice(i) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var realadvice = JSON.parse(this.responseText);
+            advices = realadvice.slip.advice;
+            console.log(advices);
+            document.getElementById("quote").innerHTML = advices;
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+var xhttp1 = new XMLHttpRequest();
+    xhttp1.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var realadvice = JSON.parse(this.responseText);
+            advices = realadvice.slip.advice;
+            console.log(advices);
+            document.getElementById("quote").innerHTML = advices;
+        }
+    };
+    xhttp1.open("GET", url, true);
+    xhttp1.send();
+
+function newAdvice(e){
+    e.preventDefault();
+
+    document.getElementById("DiscoverNewAdviceButton").addEventListener("click", getAdvice);
+
+
+    document.getElementById("homepage").style.display = "none";
+    document.getElementById("SavedAdvicePage").style.display = "none";
+    document.getElementById("NewAdvicePage").style.display = "";
+    document.getElementById("AboutPage").style.display = "none";
+
+}
+
+function savedAdvice(e){
+    e.preventDefault();
+
+    document.getElementById("homepage").style.display = "none";
+    document.getElementById("SavedAdvicePage").style.display = "";
+    document.getElementById("NewAdvicePage").style.display = "none";
+    document.getElementById("AboutPage").style.display = "none";
+
+}
+
+function goAbout(e){
+    e.preventDefault();
+
+    document.getElementById("homepage").style.display = "none";
+    document.getElementById("SavedAdvicePage").style.display = "none";
+    document.getElementById("NewAdvicePage").style.display = "none";
+    document.getElementById("AboutPage").style.display = "";
+
+}
+
+function goHome(e){
+    e.preventDefault();
+
+    document.getElementById("homepage").style.display = "";
+    document.getElementById("SavedAdvicePage").style.display = "none";
+    document.getElementById("NewAdvicePage").style.display = "none";
+    document.getElementById("AboutPage").style.display = "none";
+}
